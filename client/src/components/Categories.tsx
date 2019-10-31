e742c2178b220b60d4f9a46efe8a6b0e127a69c5
Neumeier <neumeierj@media-saturn.com>
2019-10-31 14:05:56 +0100

    I think final commit ;)

--- client/src/components/Categories.tsx
+++ client/src/components/Categories.tsx
@@ -43,17 +43,13 @@ export default class Categories extends Component {
                     <li><Link to="/" id="books, movies & music" onClick={this.handleCategorySearch} >books, movies & music</Link></li>
                     <li><Link to="/" id="electronics & computer" onClick={this.handleCategorySearch} >electronics & computer</Link></li>
                     <li><Link to="/" id="mobile phone & equipment" onClick={this.handleCategorySearch} >mobile phone & equipment</Link></li>
-                    <li><Link to="/" id="pet needs" onClick={this.handleCategorySearch} >pet needs</Link></li>
                     <li><Link to="/" id="hobby & leisure" onClick={this.handleCategorySearch} >hobby & leisure</Link></li>
-                    <li><Link to="/" id="property" onClick={this.handleCategorySearch} >property</Link></li>
-                    <li><Link to="/" id="jobs & services" onClick={this.handleCategorySearch} >jobs & services</Link></li>
                     <li><Link to="/" id="clothes & fashion" onClick={this.handleCategorySearch}>clothes & fashion</Link></li>
                     <li><Link to="/" id="furniture, housing & household" onClick={this.handleCategorySearch} >furniture, housing & household</Link></li>
                     <li><Link to="/" id="jewelry & accessories" onClick={this.handleCategorySearch} >jewelry & accessories</Link></li>
                     <li><Link to="/" id="games & consoles" onClick={this.handleCategorySearch} >games & consoles</Link></li>
                     <li><Link to="/" id="sports, travel & outdoor" onClick={this.handleCategorySearch} >sports, travel & outdoor</Link></li>
                     <li><Link to="/" id="tickets & events" onClick={this.handleCategorySearch} >tickets & events</Link></li>
-                    <li><Link to="/" id="moving & help" onClick={this.handleCategorySearch} >moving & help</Link></li>
                     <li><Link to="/" id="tools" onClick={this.handleCategorySearch} >tools</Link></li>
 
                 </ul>
